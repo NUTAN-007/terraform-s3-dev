e597e7be286f3b45886601a2b56bc47be39bc474
Nutan <nutanpasham@gmail.com>
2025-10-30 12:20:48 +0530

    removed unwanted code snipet of import

--- terraform-s3-dev/import.tf
+++ terraform-s3-dev/import.tf
@@ -1,7 +1,3 @@
-# import {
-#   to = aws_s3_bucket.create_bucket
-#   id = "my-new-bucket-name-np2710fin"
-# }
 import {
       for_each = var.enable_import ? {import_s3_bucket = var.bucket_name} : {}
       to       = aws_s3_bucket.create_bucket
